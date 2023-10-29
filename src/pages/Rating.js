be3b94698f7049b4607f87616d493ab30716b448
gorbunova.av <gorbunova.av@phystech.edu>
2023-10-30 00:14:16 +0400

    Добавлена главная страница

---
+++ src/pages/Rating.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+class Rating extends React.Component{
+    render() {
+        return (<div>
+            <Header title = "Моя успеваемость" />
+            </div>
+        )
+    }
+}
+
+export default Rating
---
+++ src/pages/Schedule.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+class Shedual extends React.Component{
+    render() {
+        return (<div>
+            <Header title = "Моё расписание" />
+            </div>
+        )
+    }
+}
+
+export default Shedual
